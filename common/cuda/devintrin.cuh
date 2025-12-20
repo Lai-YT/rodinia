25d141c9fee183a84f5acfec4d857f47929902bb
Lai-YT <381xvmvbib@gmail.com>
2025-12-20 15:52:14 +0800

    Ensure global scope is being looked up

--- common/cuda/devintrin.cuh
+++ common/cuda/devintrin.cuh
@@ -30,19 +30,19 @@ __device__ __forceinline__ unsigned int gridid() {
 template <typename T>
 __device__ __forceinline__ T tex2Ds32(cudaTextureObject_t tex, const int x,
                                       const int y) {
-  if constexpr (cuda::std::is_same_v<T, float>) {
+  if constexpr (::cuda::std::is_same_v<T, float>) {
     float a, b, c, d;
     asm volatile("tex.2d.v4.f32.s32 {%0, %1, %2, %3}, [%4, {%5, %6}];\n\t"
                  : "=f"(a), "=f"(b), "=f"(c), "=f"(d)
                  : "l"(tex), "r"(x), "r"(y));
     return a;
-  } else if constexpr (cuda::std::is_same_v<T, float2>) {
+  } else if constexpr (::cuda::std::is_same_v<T, float2>) {
     float2 a, b;
     asm volatile("tex.2d.v4.f32.s32 {%0, %1, %2, %3}, [%4, {%5, %6}];\n\t"
                  : "=f"(a.x), "=f"(a.y), "=f"(b.x), "=f"(b.y)
                  : "l"(tex), "r"(x), "r"(y));
     return a;
-  } else if constexpr (cuda::std::is_same_v<T, float4>) {
+  } else if constexpr (::cuda::std::is_same_v<T, float4>) {
     float4 a;
     asm volatile("tex.2d.v4.f32.s32 {%0, %1, %2, %3}, [%4, {%5, %6}];\n\t"
                  : "=f"(a.x), "=f"(a.y), "=f"(a.z), "=f"(a.w)
@@ -55,7 +55,7 @@ __device__ __forceinline__ T tex2Ds32(cudaTextureObject_t tex, const int x,
 
 template <typename T = float>
 __device__ __forceinline__ T tex1Ds32(cudaTextureObject_t tex, const int x) {
-  static_assert(cuda::std::is_same<T, float>::value, "Only support float type");
+  static_assert(::cuda::std::is_same<T, float>::value, "Only support float type");
   T a, b, c, d;
   asm volatile("tex.1d.v4.f32.s32 {%0, %1, %2, %3}, [%4, %5];\n\t"
                : "=f"(a), "=f"(b), "=f"(c), "=f"(d)
@@ -63,16 +63,16 @@ __device__ __forceinline__ T tex1Ds32(cudaTextureObject_t tex, const int x) {
   return a;
 }
 
-template <typename B32, cuda::std::enable_if_t<sizeof(B32) == 4, bool> = true>
+template <typename B32, ::cuda::std::enable_if_t<sizeof(B32) == 4, bool> = true>
 __device__ __forceinline__ B32 ld_global_cv(const B32 *ptr) {
-  cuda::std::uint32_t val;
+  ::cuda::std::uint32_t val;
   asm volatile("ld.global.cv.b32 %0, [%1];" : "=r"(val) : "l"(ptr));
   return reinterpret_cast<B32 &>(val);
 }
 
-template <typename B32, cuda::std::enable_if_t<sizeof(B32) == 4, bool> = true>
+template <typename B32, ::cuda::std::enable_if_t<sizeof(B32) == 4, bool> = true>
 __device__ __forceinline__ B32 ld_global_cg(const B32 *ptr) {
-  cuda::std::uint32_t val;
+  ::cuda::std::uint32_t val;
   asm volatile("ld.global.cg.b32 %0, [%1];" : "=r"(val) : "l"(ptr));
   return reinterpret_cast<B32 &>(val);
 }
